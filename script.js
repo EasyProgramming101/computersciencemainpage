96d2d908eb81b18bc8469150a71c4c6b37e863df
EasyProgramming101 <programmingmadesimple101@gmail.com>
2024-09-10 18:02:47 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("colorButton").addEventListener("click", function() {
+    const button = document.getElementById("colorButton");
+    button.style.backgroundColor = button.style.backgroundColor === 'green' ? '#007bff' : 'green';
+});
